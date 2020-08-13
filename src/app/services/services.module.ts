1170196ccb0b237f58626282c1f19eebc33d6c68
alohajt <alohajiatao@gmail.com>
2020-08-13 12:56:03 -0600

    first commit

---
+++ src/app/services/services.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { PagesModule } from '../pages/pages.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    PagesModule,
+    
+  ]
+})
+export class ServicesModule { }
