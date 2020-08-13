bedd31b621e4fd748fc18478c6ee22efef466680
alohajt <alohajiatao@gmail.com>
2020-08-13 15:53:35 -0600

    album list

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
-
+import { WyUiModule } from './wy-ui/wy-ui.module';
 
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
     NgZorroAntdModule,
     FormsModule,
-    
+    WyUiModule
   ],
   exports: [
     CommonModule,
     NgZorroAntdModule,
     FormsModule,
+    WyUiModule,
   ]
 })
 export class ShareModule { }
