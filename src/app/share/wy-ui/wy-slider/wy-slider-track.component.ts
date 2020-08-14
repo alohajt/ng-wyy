e7cc8aac7ac24e34466d074eca02490fdce1989f
alohajt <alohajiatao@gmail.com>
2020-08-13 23:23:23 -0600

    slider bar

--- src/app/share/wy-ui/wy-slider/wy-slider-track.component.ts
+++ src/app/share/wy-ui/wy-slider/wy-slider-track.component.ts
@@ -1,14 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+  
+import { Component, OnInit, Input, OnChanges, SimpleChanges, ChangeDetectionStrategy } from '@angular/core';
+import { WySliderStyle } from './wy-slider-types';
 
 @Component({
   selector: 'app-wy-slider-track',
-  template: `<div class="wy-slider-track"></div>`
+  template: `<div class="wy-slider-track" [ngStyle]="style"></div>`,
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
+  
 export class WySliderTrackComponent implements OnInit {
+  @Input() wyVertical = false;
+  @Input() wyLength: number;
+  style: WySliderStyle = {};
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['wyLength']) {
+      if (this.wyVertical) {
+        this.style.height = this.wyLength + '%';
+        this.style.left = null;
+        this.style.width = null;
+      } else {
+        this.style.width = this.wyLength + '%';
+        this.style.bottom = null;
+        this.style.height = null;
+      }
+    }
   }
 
 }
